eb90ba21f34d12bf957fd5171132a57d0647708d
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2019-04-26 17:07:43 +0300

    Initial Commit

---
+++ SignInDemo/AppDelegate.swift
@@ -0,0 +1,45 @@
+//
+//  AppDelegate.swift
+//  SignInDemo
+//
+//  Created by Алексей Пархоменко on 25/04/2019.
+//  Copyright © 2019 Алексей Пархоменко. All rights reserved.
+//
+
+import UIKit
+import GoogleSignIn
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
+    
+    
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        GIDSignIn.sharedInstance().clientID = "313857215427-1jcrlhinj8g9apbolhcqefgsjvg8s419.apps.googleusercontent.com"
+        GIDSignIn.sharedInstance().delegate = self
+        return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return GIDSignIn.sharedInstance().handle(url as URL?,
+                                                 sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
+                                                 annotation: options[UIApplication.OpenURLOptionsKey.annotation])
+    }
+
+    // MARK: GIDSignInDelegate
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        if let error = error {
+            print("\(error.localizedDescription)")
+        } else {
+            guard let fullName = user.profile.name, let image = user.profile.imageURL(withDimension: 400) else { return }
+            print("fullName: \(fullName), image: \(image)")
+        }
+    }
+
+
+}
+
---
+++ SignInDemo/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  SignInDemo
+//
+//  Created by Алексей Пархоменко on 25/04/2019.
+//  Copyright © 2019 Алексей Пархоменко. All rights reserved.
+//
+
+import UIKit
+import GoogleSignIn
+
+class ViewController: UIViewController, GIDSignInUIDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        GIDSignIn.sharedInstance()?.uiDelegate = self
+        setupSignInButton()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(signOutButtonTapped))
+    }
+    
+    fileprivate func setupSignInButton() {
+        let signInButton = GIDSignInButton(frame: CGRect(x: 0, y: 0, width: 100, height: 50))
+        signInButton.center = view.center
+        signInButton.style = .iconOnly
+        view.addSubview(signInButton)
+        
+    }
+    
+    @objc func signOutButtonTapped() {
+        GIDSignIn.sharedInstance()?.signOut()
+        print("123")
+    }
+
+
+}
+
